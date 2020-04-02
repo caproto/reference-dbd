ad10f99f088046196b55acebd06d1ae9397182b1
K Lauer <klauer@slac.stanford.edu>
2020-04-02 12:28:23 -0700

    Support generating menus as well

--- Makefile
+++ Makefile
@@ -1,17 +1,22 @@
-all: records.py
+all: records.py menus.py
 
 CAPROTO ?= $(HOME)/Repos/caproto
 
 records.py: reference.dbd
-	python generate.py reference.dbd records.py
+	python generate.py records reference.dbd records.py
 	yapf -i records.py
 	@echo "Done."
 
-copy:
+menus.py: reference.dbd
+	python generate.py menus reference.dbd menus.py
+	# yapf -i menus.py
+	@echo "Done."
+
+copy: records.py menus.py
 	cp -f records.py $(CAPROTO)/caproto/server/records.py
+	cp -f menus.py $(CAPROTO)/caproto/server/menus.py
 
 clean:
-	rm -f records.py
-
+	rm -f records.py menus.py
 
 .PHONY: all copy clean
--- generate.py
+++ generate.py
@@ -2,17 +2,27 @@ import sys
 import caproto
 import caproto.server.conversion
 
+to_generate = sys.argv[1]
+
+if to_generate not in {'menus', 'records'}:
+    print('Please specify "menus" or "records"')
+    sys.exit(1)
+
 try:
-    dbd_file = sys.argv[1]
-    output_file = sys.argv[2]
+    dbd_file = sys.argv[2]
+    output_file = sys.argv[3]
 except IndexError:
     dbd_file = 'reference.dbd'
     output_file = sys.stdout
 else:
     output_file = open(output_file, 'wt')
 
-source = caproto.server.conversion.generate_all_records_jinja(dbd_file)
+if to_generate == 'menus':
+    source = caproto.server.conversion.generate_all_menus_jinja(dbd_file)
+elif to_generate == 'records':
+    source = caproto.server.conversion.generate_all_records_jinja(dbd_file)
+
 print(f'Suggested git commit message:', file=sys.stderr)
-print(f'MNT: Generating records.py from caproto {caproto.__version__}',
+print(f'MNT: Generating {to_generate}.py from caproto {caproto.__version__}',
       file=sys.stderr)
 print(source, file=output_file)
